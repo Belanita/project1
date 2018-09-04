4f74580e3b147caa3c90058168321cc2da5807c0
Belanita <nbela8660@gmail.com>
2018-09-04 17:38:39 +0700

    project1

---
+++ App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import {Platform, StyleSheet, Text, View, Image} from 'react-native';
+import Judul from'./component/Judul';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+      <Judul/>
+        <Text style={styles.welcome}>Nama: Belanita Aniawati Ardani </Text>
+        <Text style={styles.instructions}>Kelas: XI_RPL 3 </Text>
+        <Text style={styles.instructions}>No Absen: 11</Text>
+        <Image source={require('./1.jpg')} style={{width: 200, height:200}}/>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
---
+++ component/Judul
@@ -0,0 +1,19 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+class Judul extends React.Component{
+    render(){
+        return (
+            <Text style= {salon.judul}>BIODATA</Text>
+        );
+    }
+}
+
+const salon = {
+    judul: {
+        color: "black",
+        fontSize: 50,
+        fontWeight : 'bold'
+    }
+}
+export default Judul;
